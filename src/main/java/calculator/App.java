2c3b66b38d28913a9ac02c36883a492b36cbd3e8
tichall <0817frog@gmail.com>
2024-04-28 18:17:35 +0900

    Level 1 - (4) 계산 반복 설정

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,42 +7,58 @@ public class App {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        String flagStr; // 실행 여부 받아올 변수
+        boolean flag = true; // 실행 여부 및 오류 여부를 판단하는 변수
 
-        System.out.print("첫 번째 숫자를 입력해주세요! : ");
-        int firstNum = sc.nextInt();
-
-
-        System.out.print("두 번째 숫자를 입력해주세요! : ");
-        int secondNum = sc.nextInt();
-        
-        System.out.print("사칙연산 기호를 입력해주세요! (+, -, *, /): ");
-        /* next()의 반환값은 char 타입이므로 형변환 필요! => charAt(0)으로 형변환
-        charAt()의 매개변수는 변환할 문자열의 index 값 */
-        char operator = sc.next().charAt(0);
-
-        int result = 0;
-
-        switch (operator) {
-            case '+' :
-                result = firstNum + secondNum; break;
-            case '-' :
-                result = firstNum - secondNum; break;
-            case '*' :
-                result = firstNum * secondNum; break;
-            case '/' :
-                if (secondNum == 0) {
-                    System.out.println("나눗셈에서 분모로 0을 사용할 수 없습니다.");
-                    System.exit(0);
-                } else {
-                    result = firstNum / secondNum;
-                }
-                break;
-            default:
-                System.out.println("잘못된 연산 기호를 입력하셨습니다. +, -, *, / 중 하나를 입력해주세요!");
+        while (true) {
+            if (!flag) {
+                System.out.println("시스템이 종료됩니다.");
                 System.exit(0);
+            } else {
+                System.out.print("첫 번째 숫자를 입력해주세요! : ");
+                int firstNum = sc.nextInt();
+
+                System.out.print("두 번째 숫자를 입력해주세요! : ");
+                int secondNum = sc.nextInt();
+
+                System.out.print("사칙연산 기호를 입력해주세요! (+, -, *, /): ");
+
+                /* next()의 반환값은 String 타입이므로 형변환 필요! => charAt(0)으로 형변환
+                charAt()의 매개변수는 변환할 문자열의 index 값 */
+                char operator = sc.next().charAt(0);
+
+                int result = 0;
+                switch (operator) {
+                    case '+' :
+                        result = firstNum + secondNum; break;
+                    case '-' :
+                        result = firstNum - secondNum; break;
+                    case '*' :
+                        result = firstNum * secondNum; break;
+                    case '/' :
+                        if (secondNum == 0) {
+                            System.out.println("나눗셈에서 분모로 0을 사용할 수 없습니다.");
+                            flag = false;
+                        } else {
+                            result = firstNum / secondNum;
+                        }
+                        break;
+                    default:
+                        System.out.println("잘못된 연산 기호를 입력하셨습니다. +, -, *, / 중 하나를 입력해주세요!");
+                        flag = false;
+                }
+
+                if (flag) {
+                    System.out.println("결과 : " + result);
+                }
+
+                flag = true;
+                System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
+                flagStr = sc.next();
+                if (flagStr.equals("exit")) {
+                    flag = false;
+                }
+            }
         }
-
-        System.out.println("결과 : " + result);
-
     }
 }
