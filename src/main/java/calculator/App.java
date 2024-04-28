a35cb32b581bd84511a8beca74495595ae6ac6a7
tichall <0817frog@gmail.com>
2024-04-28 17:05:27 +0900

    Level 1 - (1) 두ì 개의 정수 입력받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,5 +8,14 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("첫 번쨰 숫자를 입력해주세요! : ");
+        int firstNum = sc.nextInt();
+
+        System.out.print("두 번째 숫자를 입력해주세요! : ");
+        int secondNum = sc.nextInt();
+
+        System.out.println("firstNum, secondNum = " +firstNum + ","+ secondNum);
+
+
     }
 }
