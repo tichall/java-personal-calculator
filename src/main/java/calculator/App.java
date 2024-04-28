60548277107732997d8c4b12d84f2e2cd67943e8
tichall <0817frog@gmail.com>
2024-04-28 17:39:39 +0900

    Level 1 - (3) 계산 기능 구현

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,17 +8,44 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번쨰 숫자를 입력해주세요! : ");
+        System.out.print("첫 번째 숫자를 입력해주세요! : ");
         int firstNum = sc.nextInt();
 
 
         System.out.print("두 번째 숫자를 입력해주세요! : ");
         int secondNum = sc.nextInt();
         
-        System.out.print("사칙연산 기호를 입력해주세요! : ");
+        System.out.print("사칙연산 기호를 입력해주세요! (+, -, *, /): ");
         /* next()의 반환값은 char 타입이므로 형변환 필요! => charAt(0)으로 형변환
         charAt()의 매개변수는 변환할 문자열의 index 값 */
         char operator = sc.next().charAt(0);
-        System.out.println("operator = " + operator); // +, -, *, /
+
+        int result = 0;
+        boolean state = true;
+        switch (operator) {
+            case '+' :
+                result = firstNum + secondNum; break;
+            case '-' :
+                result = firstNum - secondNum; break;
+            case '*' :
+                result = firstNum * secondNum; break;
+            case '/' :
+                if (secondNum == 0) {
+                    System.out.println("나눗셈에서 분모로 0을 사용할 수 없습니다.");
+                    state = false;
+                } else {
+                    result = firstNum / secondNum;
+                }
+                break;
+            default:
+                System.out.println("잘못된 연산 기호를 입력하셨습니다. +, -, *, / 중 하나를 입력해주세요!");
+                state = false;
+                break;
+        }
+        if (state == true) {
+            System.out.println("결과 : " + result);
+        } else {
+            System.out.println("입력 오류로 프로그램이 종료됩니다.");
+        }
     }
 }
