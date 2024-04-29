ce27d6c4680238dfe582c6471419bb523d29bc7e
tichall <0817frog@gmail.com>
2024-04-29 11:49:34 +0900

    Level 1 - (7) 컬렉션으로 변경 및 결과값 삭제 기능

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,10 +5,10 @@ public class App {
 
     public static void main(String[] args) {
         ArrayList<Integer> resultArr = new ArrayList<Integer>();
-        String flagStr; // 실행 여부 받아올 변수
-        boolean flag = true; // 실행 여부 및 오류 여부를 판단하는 변수
-        boolean errFlag;
-        int result = 0;
+        String flagStr; // 실행 여부 메시지
+        boolean flag = true; // 실행 여부를 판단하는 변수
+        boolean errFlag; // 오류 여부 판단하는 변수
+        int result = 0; // 연산 결과값
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -60,9 +60,10 @@ public class App {
                     resultArr.add(result);
                 }
 
+                System.out.println("==================");
+
                 // 결과값 리스트에 값이 존재할 때만 삭제 메시지 표시
                 if (!resultArr.isEmpty()) {
-                    System.out.println("==================");
                     System.out.print("첫 번째로 저장된 결과값을 삭제하시겠습니까? (remove 입력 시 삭제) : ");
                     String selectRemove = sc.next();
                     if (selectRemove.equals("remove")) {
@@ -71,7 +72,6 @@ public class App {
                         System.out.println("결과값 " + removeNum + " 삭제 완료! (남은 결과값 개수 " + resultArr.size() + "개)");
                     }
                 } else {
-                    System.out.println("==================");
                     System.out.println("저장된 결과값이 없습니다.");
                 }
 
