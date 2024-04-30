2717c7772dbf1907b336f4079a4c33376b5535f3
tichall <0817frog@gmail.com>
2024-04-30 22:45:31 +0900

    Level 2 - (7) 원의 넓이 계산 메서드 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,70 +8,100 @@ public class App {
         boolean flag = true; // 실행 여부를 판단하는 변수
         boolean errFlag; // 오류 여부 판단하는 변수
 
-        Calculator cal = new Calculator(new ArrayList<>());
-        int result = 0;
+        Calculator arithCal = new Calculator(new ArrayList<>());
+        Calculator circleCal = new Calculator(new ArrayList<>(), "circle");
+
+        double result = 0;
 
         Scanner sc = new Scanner(System.in);
 
         while (true) {
+
             if (!flag) {
                 System.out.println("프로그램이 종료됩니다.");
                 System.exit(0);
             } else {
-                errFlag = false; // errFlag 초기화
-                System.out.print("첫 번째 숫자를 입력해주세요! : ");
-                int firstNum = sc.nextInt();
+                System.out.print("계산기를 선택해주세요! [1. 원의 넓이 계산기 (1 입력)] [2. 사칙연산 계산기 (2 입력)] : ");
+                String option = sc.next();
+                switch (option) {
+                    // 원의 넓이 계산기 선택 시
+                    case "1" :
+                        System.out.print("원의 반지름을 입력해주세요! : ");
+                        int radius = sc.nextInt();
+                        if (radius < 0) {
+                            System.out.println("양의 정수만 계산 가능합니다.");
+                        } else {
+                            result = circleCal.calculateCircleArea(radius);
+                            circleCal.setCircleResultArr(result);
+                            System.out.println("원의 넓이 : " + result + " (저장 완료!)");
 
-                System.out.print("두 번째 숫자를 입력해주세요! : ");
-                int secondNum = sc.nextInt();
+                            System.out.println("==================");
+                            circleCal.inquiryCircleResults();
+                        }
+                        break;
 
-                // 양의 정수만 입력 가능
-                if (firstNum < 0 || secondNum < 0) {
-                    System.out.println("양의 정수만 계산 가능합니다!");
-                    errFlag = true;
-                } else {
-                    System.out.print("사칙연산 기호를 입력해주세요! (+, -, *, /): ");
+                    // 사칙연산 계산기 선택 시
+                    case "2" :
+                        errFlag = false; // errFlag 초기화
+                        System.out.print("첫 번째 숫자를 입력해주세요! : ");
+                        int firstNum = sc.nextInt();
 
-                    char operator = sc.next().charAt(0);
-                    try {
-                        result = cal.calculate(firstNum, secondNum, operator);
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                        errFlag = true;
-                    }
+                        System.out.print("두 번째 숫자를 입력해주세요! : ");
+                        int secondNum = sc.nextInt();
+
+                        // 양의 정수만 입력 가능
+                        if (firstNum < 0 || secondNum < 0) {
+                            System.out.println("양의 정수만 계산 가능합니다!");
+                            errFlag = true;
+                        } else {
+                            System.out.print("사칙연산 기호를 입력해주세요! (+, -, *, /): ");
+
+                            char operator = sc.next().charAt(0);
+                            try {
+                                result = arithCal.calculate(firstNum, secondNum, operator);
+                            } catch (Exception e) {
+                                System.out.println(e.getMessage());
+                                errFlag = true;
+                            }
+                        }
+
+                        // 계산 중 오류가 없는 경우만 결과 출력 및 결과값 저장
+                        if (!errFlag) {
+                            arithCal.setArithResultArr((int) result);
+                            System.out.println("결과 : " + (int) result + " (저장 완료!)");
+                        }
+
+                        System.out.println("==================");
+
+                        // 결과값 리스트에 값이 존재할 때만 삭제, 조회 메시지 표시
+                        if (!arithCal.getArithResultArr().isEmpty()) {
+                            System.out.print("첫 번째로 저장된 결과값을 삭제하시겠습니까? (remove 입력 시 삭제) : ");
+                            String selectRemove = sc.next();
+                            if (selectRemove.equals("remove")) {
+                                //int removeNum = cal.resultArr.get(0);
+
+                                arithCal.removeResult();
+                                System.out.println("삭제 완료!");
+                                //System.out.println("결과값 " + removeNum + " 삭제 완료! (남은 결과값 개수 " + cal.getResultArrSize() + "개)");
+                            }
+
+                            System.out.println("==================");
+                            System.out.print("저장된 결과값을 조회하시겠습니까? (inquiry 입력 시 조회) : ");
+                            String selectView = sc.next();
+                            if (selectView.equals("inquiry")) {
+                                System.out.println("==================");
+                                arithCal.inquiryResults();
+                            }
+
+                        } else {
+                            System.out.println("저장된 결과값이 없습니다.");
+                        }
+                        break;
+                    // 1,2 이외의 다른 값을 입력한 경우
+                    default :
+                        System.out.println("잘못된 옵션을 선택하셨습니다. 1 또는 2를 입력해주세요!");
+                        break;
                 }
-
-                // 계산 중 오류가 없는 경우만 결과 출력 및 결과값 저장
-                if (!errFlag) {
-                    cal.setResultArr(result);
-                    System.out.println("결과 : " + result + " (저장 완료!)");
-                }
-
-                System.out.println("==================");
-
-                // 결과값 리스트에 값이 존재할 때만 삭제, 조회 메시지 표시
-                if (!cal.getResultArr().isEmpty()) {
-                    System.out.print("첫 번째로 저장된 결과값을 삭제하시겠습니까? (remove 입력 시 삭제) : ");
-                    String selectRemove = sc.next();
-                    if (selectRemove.equals("remove")) {
-                        //int removeNum = cal.resultArr.get(0);
-
-                        cal.removeResult();
-                        System.out.println("삭제 완료!");
-                        //System.out.println("결과값 " + removeNum + " 삭제 완료! (남은 결과값 개수 " + cal.getResultArrSize() + "개)");
-                    }
-
-                    System.out.println("==================");
-                    System.out.print("저장된 결과값을 조회하시겠습니까? (inquiry 입력 시 조회) : ");
-                    String selectView = sc.next();
-                    if (selectView.equals("inquiry")) {
-                        cal.inquiryResults();
-                    }
-
-                } else {
-                    System.out.println("저장된 결과값이 없습니다.");
-                }
-
                 System.out.println("==================");
 
                 System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,12 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    private List<Integer> resultArr;
-    int result = 0;
+    // 우선 Calculator 객체가 여러 개 생성됨에 따라 각 결과값 리스트는 독립적으로 운용되어야 하므로 static 키워드를 붙이는 건 부적절해보인다. 계속 리스트 내부가 수정되므로 final 키워드도 부적절하다.
+    private List<Integer> arithResultArr;
+    private List<Double> circleResultArr;
 
-    public Calculator(List<Integer> resultArr) {
-        this.resultArr =  resultArr;
+    // result 값은 계속 수정되기 때문에 final은 불가능, result 변수가 객체마다 꼭 하나씩 필요한 건 아닌 것 같아보이면서도 static을 안 붙이는 편이 이해상 더 좋아보이는 느낌이 든다.
+    double result;
+
+    public Calculator(List<Integer> arithResultArr) {
+        this.arithResultArr =  arithResultArr;
     }
+
+    // 사용하지 않을 type 값을 받아서 생성자를 정의하는 것이 좋지 않아보인다. 하지만 같은 형식의 컬렉션 두 개를 어떻게 구분해서 매개변수로 전달하고 클래스 내부 컬렉션에 할당해줘야 할 지 감이 잘 안 와서 이런 방법을 사용하게 되었다.
+    public Calculator(List<Double> circleResultArr, String type) {
+        this.circleResultArr =  circleResultArr;
+    }
+
+
     public int calculate(int firstNum, int secondNum, char operator) throws Exception {
 
         switch (operator) {
@@ -26,27 +37,45 @@ public class Calculator {
                 // 잘못된 연산자 예외 처리
                 throw new OperationException(operator);
         }
-        return this.result;
-    }
-    public List<Integer> getResultArr () {
-        return this.resultArr;
-    }
-    public int getResultArrSize() {
-        return this.resultArr.size();
+        return (int) this.result;
     }
 
-    public void setResultArr(int result) {
-            this.resultArr.add(result);
+    //
+    public List<Integer> getArithResultArr () {
+        return this.arithResultArr;
+    }
+
+    public void setArithResultArr(int result) {
+            this.arithResultArr.add(result);
     }
 
     public void removeResult() {
-        this.resultArr.remove(0);
+        this.arithResultArr.remove(0);
     }
 
     public void inquiryResults() {
-        for (Integer i : this.resultArr) {
+        System.out.println("[ 총 결과값 개수 : " + getArithResultArr().size() + " ]");
+        for (Integer i : this.arithResultArr) {
             System.out.println(i);
         }
-        System.out.println("[ 총 결과값 개수 : " + getResultArrSize() + " ]");
+    }
+
+    // 원의 넓이 계산 후 반납
+    public double calculateCircleArea(int radius) {
+        this.result = radius * 3.14;
+        return this.result;
+    }
+    public List<Double> getCircleResultArr() {
+        return this.circleResultArr;
+    }
+
+    public void setCircleResultArr(double result) {
+        this.circleResultArr.add(result);
+    }
+    public void inquiryCircleResults() {
+        System.out.println("[ 총 결과값 개수 : " + getCircleResultArr().size() + " ]");
+        for (Double i : this.circleResultArr) {
+            System.out.println(i);
+        }
     }
 }
